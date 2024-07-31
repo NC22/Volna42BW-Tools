b50d305c9a1aade27d486ac6a60eb1d371db3030
NC22 <torrenttvi@gmail.com>
2024-07-31 03:17:21 +0300

    remove test generation

--- htmlPack/cpp/out/test.h
+++
@@ -1,2 +0,0 @@
-const char webdata_test_html[] PROGMEM = "<html><head><style>.test {	border : 1px;}</style><script>var test = 1;</script></head></html>";
-const unsigned int webdataSize_test_html PROGMEM = 93;
